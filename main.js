13cabe4ad920c798d635f29be61422bf2e1a31e8
Piotr <piorus9196@gmail.com>
2020-02-12 09:03:50 +0100

    menu aside added

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-const hamburger = document.querySelector('.hamburger')
-const menu = document.querySelector('.menu')
-const desktop = document.querySelector('.desktop')
-const add = () => {
-    menu.classList.toggle("aside")
-    desktop.classList.toggle("opacity")
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const container = document.querySelector('.container');
+const lorem = document.querySelector('.lorem');
+const table = document.querySelector('.table');
+const tableBtns = document.querySelectorAll('.tabele-btn');
+
+
+const show = () => {
+    lorem.classList.toggle("none");
+    table.classList.toggle("none");
+
 }
+const add = () => {
+    menu.classList.toggle("aside");
+    container.classList.toggle("opacity");
+};
+
+tableBtns.forEach(btn => {
+    btn.addEventListener('click', show);
+});
 hamburger.addEventListener('click', add);
